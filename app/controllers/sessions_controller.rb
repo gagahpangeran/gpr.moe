383c3fcf1f29f2d16f853741d44e2fcf67904d29
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-08-12 00:07:11 +0700

    add notice after login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:username, :password))
       start_new_session_for user
-      redirect_to after_authentication_url
+      redirect_to after_authentication_url, notice: "You are logged in."
     else
       redirect_to login_path, alert: "Wrong username or password."
     end
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     terminate_session
-    redirect_to root_path
+    redirect_to root_path, notice: "You are logged out."
   end
 end
