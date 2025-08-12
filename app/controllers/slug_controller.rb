2eb4ec8fb3fbfeb380367ca627cd023907d39844
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-08-12 20:04:32 +0700

    change slug controller to handle rate limit and not found link

--- app/controllers/slug_controller.rb
+++ app/controllers/slug_controller.rb
@@ -1,14 +1,10 @@
 class SlugController < ApplicationController
   allow_unauthenticated_access
+  rate_limit to: 10, within: 1.minutes, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def visit
-    link = Link.find_by(slug: params[:slug])
-
-    if link
-      link.increment!(:visit_count)
-      redirect_to link.url, status: :moved_permanently, allow_other_host: true
-    else
-      head :not_found
-    end
+    link = Link.find_by!(slug: params[:slug])
+    link.increment!(:visit_count)
+    redirect_to link.url, status: :moved_permanently, allow_other_host: true
   end
 end
