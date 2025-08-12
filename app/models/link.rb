4deb9a1f7c03b40c8f24435192878b22d0ebc38c
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-08-12 12:49:29 +0700

    reset visit count when link is updated

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,11 @@ class Link < ApplicationRecord
 
   validates :slug, presence: { message: "can not empty" },
                     uniqueness: { case_sensitive: false, message: "already exists" }
+
+  before_update :reset_visit_count
+
+  private
+    def reset_visit_count
+      self.visit_count = 0
+    end
 end
