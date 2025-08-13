4e40031111a4d9402e5abd4b9aacba26ea74c240
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-08-13 10:50:34 +0700

    update link test to setup user

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,14 +1,17 @@
 require "test_helper"
 
 class LinkTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:one)
+  end
+
   test "valid link" do
     link = links(:one)
     assert link.valid?
   end
 
   test "insert new valid link" do
-    user = users(:one)
-    link = Link.create(slug: "r4Nd0m", url: "https://random.site", user: user)
+    link = Link.create(slug: "r4Nd0m", url: "https://random.site", user: @user)
 
     assert link.valid?
     assert_equal "r4Nd0m", link.slug
@@ -17,36 +20,30 @@ class LinkTest < ActiveSupport::TestCase
   end
 
   test "insert empty slug" do
-    user = users(:one)
-
     assert_raises ActiveRecord::RecordInvalid do
-      Link.create!(slug: "", url: "https://random.site", user: user)
+      Link.create!(slug: "", url: "https://random.site", user: @user)
     end
   end
 
   test "insert nil slug" do
-    user = users(:one)
-
     assert_raises ActiveRecord::RecordInvalid do
-      Link.create!(slug: nil, url: "https://random.site", user: user)
+      Link.create!(slug: nil, url: "https://random.site", user: @user)
     end
   end
 
   test "insert duplicate slug" do
     slug = links(:one).slug
-    user = users(:one)
 
     assert_raises ActiveRecord::RecordInvalid do
-      Link.create!(slug: slug, url: "https://random.site", user: user)
+      Link.create!(slug: slug, url: "https://random.site", user: @user)
     end
   end
 
   test "insert duplicate slug with different case" do
     slug = links(:one).slug.upcase
-    user = users(:one)
 
     assert_raises ActiveRecord::RecordInvalid do
-      Link.create!(slug: slug, url: "https://random.site", user: user)
+      Link.create!(slug: slug, url: "https://random.site", user: @user)
     end
   end
 end
