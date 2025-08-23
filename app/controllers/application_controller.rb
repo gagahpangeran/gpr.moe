008eac8c1f824a9a5c08a77bd8e0111f729c4e67
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-08-23 21:17:41 +0700

    increase rate limit value

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Authentication
   include Authorization
 
-  rate_limit to: 10, within: 1.minutes
+  rate_limit to: 100, within: 1.minutes
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
